8cc795d698d852ac98e8b891e8a9780d60dca029
Madusudhanan <madhusona@gmail.com>
2019-09-28 14:01:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,10 +3,10 @@ self.addEventListener('install', function(e) {
     e.waitUntil(
       caches.open('your-magic-cache').then(function(cache) {
         return cache.addAll([
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/site.js',
+          './',
+          './index.html',
+          './manifest.json',
+          './site.js',
           ]);
       })
     );
