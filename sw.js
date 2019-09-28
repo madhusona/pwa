4c09b79e97b81c2c19951da4a7ea630eff34523f
Madusudhanan <madhusona@gmail.com>
2019-09-28 13:36:08 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+/** An empty service worker! */
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+      caches.open('your-magic-cache').then(function(cache) {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/manifest.json',
+          '/site.js',
+          ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  });
